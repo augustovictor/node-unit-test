c2ed20ff3d23b7a79da4daedbf82704769946aec
Victor <victoraweb@gmail.com>
2016-03-24 02:38:40 -0300

    Using expect

--- test.js
+++ test.js
@@ -12,8 +12,11 @@ function isEven(num) {
     return num % 2 === 0;
 };
 
-describe('isEven', function() {
+describe('isEven()', function() {
     it('should return true when number is even', function() {
         isEven(4).should.be.true;
+    });
+    it('should return false when number is odd', function() {
+        expect(isEven(3)).to.be.false;
     });
 });
