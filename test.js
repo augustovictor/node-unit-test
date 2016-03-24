08dcf6128bd439d7ee9c21c895f826d37431d83d
Victor <victoraweb@gmail.com>
2016-03-24 02:47:27 -0300

    Using beforeEach()

--- test.js
+++ test.js
@@ -12,11 +12,33 @@ function isEven(num) {
     return num % 2 === 0;
 };
 
+function add(num1, num2) {
+    return num1 + num2;
+}
+
 describe('isEven()', function() {
     it('should return true when number is even', function() {
         isEven(4).should.be.true;
     });
     it('should return false when number is odd', function() {
         expect(isEven(3)).to.be.false;
+    });
+});
+
+describe('add()', function() {
+    var num = 5;
+
+    beforeEach(function() {
+        num = 5;
+    });
+
+    it('should return 10 when adding 5 to 5', function() {
+        num = add(num,5)
+        num.should.equal(10);
+    });
+
+    it('should return 12 when adding 7 to 5', function() {
+        num = add(num, 7);
+        num.should.equal(12);
     });
 });
