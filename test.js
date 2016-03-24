0a72e51d56d52cace9b0d34abca180d7e807115e
Victor <victoraweb@gmail.com>
2016-03-24 02:51:37 -0300

    Skip and only

--- test.js
+++ test.js
@@ -16,29 +16,33 @@ function add(num1, num2) {
     return num1 + num2;
 }
 
-describe('isEven()', function() {
-    it('should return true when number is even', function() {
-        isEven(4).should.be.true;
-    });
-    it('should return false when number is odd', function() {
-        expect(isEven(3)).to.be.false;
-    });
-});
-
-describe('add()', function() {
-    var num = 5;
-
-    beforeEach(function() {
-        num = 5;
+describe('Number tests', function() {
+    describe('isEven()', function() {
+        it('should return true when number is even', function() {
+            isEven(4).should.be.true;
+        });
+        it('should return false when number is odd', function() {
+            expect(isEven(3)).to.be.false;
+        });
     });
 
-    it('should return 10 when adding 5 to 5', function() {
-        num = add(num,5)
-        num.should.equal(10);
-    });
+    // xdescribe('add()', function() {
+    describe('add()', function() {
+        var num = 5;
 
-    it('should return 12 when adding 7 to 5', function() {
-        num = add(num, 7);
-        num.should.equal(12);
+        beforeEach(function() {
+            num = 5;
+        });
+
+        // it.only('should return 10 when adding 5 to 5', function() {
+        it('should return 10 when adding 5 to 5', function() {
+            num = add(num,5)
+            num.should.equal(10);
+        });
+
+        xit('should return 12 when adding 7 to 5', function() {
+            num = add(num, 7);
+            num.should.equal(12);
+        });
     });
 });
