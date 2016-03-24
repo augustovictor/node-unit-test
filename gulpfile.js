b3f36e8d5153c934a8341c96496c5faeabedb275
Victor <victoraweb@gmail.com>
2016-03-24 10:43:02 -0300

    Setting watch-mocha task

--- gulpfile.js
+++ gulpfile.js
@@ -11,4 +11,14 @@ gulp.task('mocha', function() {
     return gulp.src(['test/*.js'], { read: false})
         .pipe(mocha( { reporter: 'list' } ))
         .on('error', gutil.log);
-})
+});
+
+gulp.task('watch-mocha', function() {
+    gulp.run('mocha');
+
+    // Watch files and run mocha task when changes occur
+    gulp.watch(['./**/*.js', 'test/**/*.js'], ['mocha']);
+});
+
+// It tells gulp if we call 'gulp' in our command line with no parameters this task will be executed
+gulp.task('default', ['watch-mocha'])
