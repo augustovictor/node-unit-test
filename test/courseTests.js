bbb11eff460d46576b9cef68c55635e5ce84846c
Victor <victoraweb@gmail.com>
2016-03-26 01:27:30 -0300

    Testing course [business logic]

--- test/courseTests.js
+++ test/courseTests.js
@@ -9,5 +9,28 @@ var Student = require("../Student")
 ;
 
 describe("Course", function () {
-  
+  var
+    courseName = 'Introduction to awesomeness',
+    courseCode = 'AWE 101',
+    courseDescription = 'This course will make you awesome!'
+  ;
+
+  it('should save data correctly', function() {
+      var course = Course.create(courseName, courseCode, courseDescription);
+
+      should.exist(course.name);
+      course.name.should.equal(courseName);
+
+      should.exist(course.code);
+      course.code.should.equal(courseCode);
+
+      should.exist(course.description);
+      course.description.should.equal(courseDescription);
+
+      should.exist(course.students);
+      course.students.should.eql([]); // equal() compares references whereas eql() compare values
+
+      should.exist(course.times);
+      course.times.should.eql([]);
+  });
 });
