6e180174d966d22c7420982d308b5b469755b9fb
Victor <victoraweb@gmail.com>
2016-03-27 12:39:49 -0300

    Testing result of promise

---
+++ test/promisesTests.js
@@ -0,0 +1,34 @@
+/// <reference path="typings/mocha/mocha.d.ts"/>
+'use strict';
+
+var Promise = require('bluebird');
+var chai = require('chai');
+// var chaiAsPromised = require('chai-as-promised');
+var should = chai.should();
+
+// chai.use(chaiAsPromised);
+
+var student = {name: 'John Doe', id: 3};
+
+var dataAccess = {
+  getStudent: function(id) {
+    if (id === 3) {
+      return Promise.resolve(student);
+    } else {
+      return Promise.reject('Invalid Student Id');
+    }
+  }
+};
+
+describe('getStudent', function() {
+  it('uses the done function', function(done) {
+    dataAccess.getStudent(3).then(function(student) {
+      student.id.should.equal(3);
+      done();
+    });
+  });
+
+  it('fulfills the promise', function() {
+    return dataAccess.getStudent(3);
+  });
+});
